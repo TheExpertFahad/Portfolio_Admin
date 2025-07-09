a484b3ee2a1441bb7d2b166871dcf58ce3820e75
Fahad Ali <158840148+TheExpertFahad@users.noreply.github.com>
2025-07-09 15:14:25 +0500

    Update WebsiteRoutes.js

--- Routes/WebsiteRoutes/WebsiteRoutes.js
+++ Routes/WebsiteRoutes/WebsiteRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { GetEducationById, GetAllEducation } = require('../../Controller/AdminController/EducationController');
+const { GetAllTestimonial } = require('../../Controller/AdminController/TestimonialController');
 const router = express.Router();
 
 
 
 router.get("/education/:id", GetEducationById);
 router.get("/education-get-all", GetAllEducation);
+router.get("/testimonial-get-all", GetAllTestimonial);
 
 router.get('/', (req, res) => {
     res.send('Hello from Router!');
